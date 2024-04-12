084d42dd8db0e8ce528c742b56d090bbbecdd6f9
Vlad Chmara <chmaravlad5@gmail.com>
2024-04-12 21:57:36 +0300

    updated blog routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,17 +6,26 @@ const Blog = mongoose.model('Blog');
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+      _user: req.user?.id,
+      _id: req.params?.id
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
-    res.send(blogs);
+    try {
+      const blogs = await Blog.find({ _user: req.user?.id });
+      if(blogs) {
+        res.send(blogs)
+        return
+      };
+      res.send([{title: 'hard coded title', content: 'hard coded no text'}])
+      
+    } catch (error) {
+      res.status(500).send(error)
+    }
+    
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
@@ -25,14 +34,14 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user?.id
     });
 
     try {
       await blog.save();
       res.send(blog);
     } catch (err) {
-      res.send(400, err);
+      res.status(400).send(err)
     }
   });
 };
